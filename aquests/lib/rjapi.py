fb30be689f225f647342ec00408ea975869c9f40
Hans Roh <hansroh@gmail.com>
2017-04-13 21:21:37 +0900

    make rjapi sessioned requests for keep-alive

--- aquests/lib/rjapi.py
+++ aquests/lib/rjapi.py
@@ -1,9 +1,11 @@
 import requests
 import json
+from urllib.parse import quote
 
 def tostr (p):
 	if p:
-		return dict ([(k, str (v)) for k, v in p.items ()])	
+		return "?" + "&".join (["%s=%s" % (k, quote (str (v))) for k, v in p.items ()])	
+	return ""
 			
 class API:
 	API_SERVER = "http://127.0.0.1:5000"
@@ -12,16 +14,17 @@ class API:
 	def __init__ (self, server, timeout = 30.0):
 		self.API_SERVER = server
 		self.REQ_TIMEOUT = timeout
+		self.session = requests.Session ()
 			
 	def post (self, uri, d):
-		return requests.post ("%s/%s" % (self.API_SERVER, uri), json.dumps (d), timeout = self.REQ_TIMEOUT).json ()
+		return self.session.post ("%s/%s" % (self.API_SERVER, uri), json.dumps (d), timeout = self.REQ_TIMEOUT).json ()
 	
 	def put (self, uri, d):
-		return requests.put ("%s/%s" % (self.API_SERVER, uri), json.dumps (d), timeout = self.REQ_TIMEOUT).json ()
+		return self.session.put ("%s/%s" % (self.API_SERVER, uri), json.dumps (d), timeout = self.REQ_TIMEOUT).json ()
 	
 	def get (self, uri, p = {}):	
-		return requests.get ("%s/%s" % (self.API_SERVER, uri), tostr (p), timeout = self.REQ_TIMEOUT).json ()
+		return self.session.get ("%s/%s%s" % (self.API_SERVER, uri, tostr (p)), timeout = self.REQ_TIMEOUT).json ()
 	
-	def delete (self, uri):
-		return requests.delete ("%s/%s" % (self.API_SERVER, uri), timeout = self.REQ_TIMEOUT).json ()
+	def delete (self, uri, p = {}):
+		return self.session.delete ("%s/%s%s" % (self.API_SERVER, uri, tostr (p)), timeout = self.REQ_TIMEOUT).json ()
 
