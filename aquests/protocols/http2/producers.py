8e60ea14834cff557daf9ee1e5c7cd26015c329a
Hhans Roh <hansroh@gmail.com>
2017-08-06 15:05:46 +0900

    unify h2 producers

--- aquests/protocols/http2/producers.py
+++ aquests/protocols/http2/producers.py
@@ -26,7 +26,7 @@ class h2header_producer:
 		return "<h2header_producer stream_id:%d>" % (self.__stream_id,)
 	
 	def more (self):		
-		data_to_send, self.data_to_send = self.data_to_send, b''
+		data_to_send, self.data_to_send = self.data_to_send, b''		
 		return data_to_send
 
 
@@ -44,97 +44,71 @@ class h2frame_producer:
 		self._buf = b""
 		self._end_stream = False		
 		self._last_sent = time.time ()
+		self._ready = None
+		self._frame = b''
 		
 		if hasattr (producer, "ready"):
-			self.ready = producer.ready
+			self._ready = producer.ready
 			producer.ready = None
 	
+	def ready (self):
+		#print ('++++++++++++++ready', len (self._frame), self.is_done ())
+		if self.is_done ():
+			return True
+			
+		if self._frame:
+			# data or reset
+			return True
+		
+		lfcw = self.encoder.local_flow_control_window (self.stream_id)
+		#print ("---MULTIPLEXING", self.stream_id, lfcw)
+		
+		if lfcw == 0:
+			# flow control error, graceful close
+			if time.time () - self._last_sent > 10:
+				 self.encoder.reset_stream (stream_id, error_code = errcode)
+				 self._frame = self.encoder.data_to_send ()
+				 self._buf = b''
+				 self._end_stream = True
+				 return True
+			return False
+		
+		avail_data_length = min (self.SIZE_BUFFER, lfcw)
+		if not self._buf:
+			if self._ready and not self._ready ():
+				return False				
+			self._buf = self.producer.more ()
+			self._end_stream = self.is_end_stream (self._buf)			
+		data, self._buf = self._buf [:avail_data_length], self._buf [avail_data_length:]
+		
+		# try build frame
+		with self._lock:		
+			self.encoder.send_data (
+				stream_id = self.stream_id,
+				data = data,
+				end_stream = self._end_stream and not self._buf
+			)
+			self._frame = self.encoder.data_to_send ()
+			
+		return True
+		
 	def get_size (self):			
-		return self.producer.get_size ()
+		return self._ready and -1 or self.producer.get_size ()
 		
 	def __repr__ (self):
 		return "<h2frame_producer stream_id:%d, weight:%d, depends_on:%d>" % (self.stream_id, self.weight, self.depends_on)
 	
 	def is_done (self):
-		return self._end_stream and not self._buf
+		return self._end_stream and not self._buf and not self._frame
 	
 	def is_end_stream (self, data):
-		return not data or len (data) < self.producer.buffer_size
+		return self._ready and len (data) == 0 or len (data) < self.producer.buffer_size
 				 	
 	def more (self):
+		#print ('++++++++++++++more', len (self._frame), self.is_done ())
+		self._last_sent = time.time ()
 		if self.is_done ():
 			return b''
-
-		if self._buf:
-			data, self._buf = self._buf [:self.SIZE_BUFFER], self._buf [self.SIZE_BUFFER:]
-				
-		else:
-			data = self.producer.more ()
-			self._end_stream = self.is_end_stream (data)
-			if len (data) > self.SIZE_BUFFER:
-				data, self._buf = data [:self.SIZE_BUFFER], data [self.SIZE_BUFFER:]
-		
-		# print ("MULTIPLEXING", self.stream_id, self.encoder.local_flow_control_window (self.stream_id))
-		with self._lock:
-			try:
-				self.encoder.send_data (
-					stream_id = self.stream_id,
-					data = data,
-					end_stream = self.is_done ()
-				)
-				self._last_sent = time.time ()	
-				data_to_send = self.encoder.data_to_send ()
-				
-			except FlowControlError:
-				# close forcely
-				return b''
-		
-		return data_to_send
+		frame, self._frame = self._frame, b''		
+		return frame
 	
-	
-class h2stream_producer (h2frame_producer):
-	def __repr__ (self):
-		return "<h2stream_producer stream_id:%d>" % (self.stream_id,)
-	
-	def get_size (self):			
-		return -1
-		
-	def is_end_stream (self, data):
-		return len (data) == 0
-		
-	def more (self):
-		if self.is_done ():
-			return b''
-
-		if self._buf:
-			data, self._buf = self._buf [:self.SIZE_BUFFER], self._buf [self.SIZE_BUFFER:]
-				
-		else:
-			data = self.producer.more ()
-			self._end_stream = self.is_end_stream (data)
-			if len (data) > self.SIZE_BUFFER:
-				data, self._buf = data [:self.SIZE_BUFFER], data [self.SIZE_BUFFER:]
-		
-		with self._lock:
-			try:
-				self.encoder.send_data (
-					stream_id = self.stream_id,
-					data = data,
-					end_stream = self.is_done ()
-				)
-				self._last_sent = time.time ()	
-				data_to_send = self.encoder.data_to_send ()
-				
-			except FlowControlError:
-				# close forcely
-				return b''
-		
-		return data_to_send
-
-class h2_globbing_producer (producers.globbing_producer):
-	def __init__ (self, stream_id, depends_on, weight,  producer, buffer_size = producers.SIZE_BUFFER):
-		self.stream_id = stream_id
-		self.depends_on = depends_on
-		self.weight = weight
-		producers.globbing_producer.__init__ (self, producer, buffer_size)
-		
