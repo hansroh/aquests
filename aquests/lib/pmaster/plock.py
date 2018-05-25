127fb01bf2b558f67e413bcd445beff7f6ad5786
Hans Roh <hansroh@gmail.com>
2018-05-25 23:32:54 +0900

    tt

--- aquests/lib/pmaster/plock.py
+++ aquests/lib/pmaster/plock.py
@@ -20,4 +20,11 @@ if os.name == "posix":
 			
 		def __del__(self):
 			self.handle.close()
+		
+		def __enter__ (self):
+			self.acquire ()
+			return self
 	
+		def __exit__ (self, type, value, tb):
+			self.release ()
+			
