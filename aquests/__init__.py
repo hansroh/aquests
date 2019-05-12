9d18f901b2c516fb620a6eac0489f0f95e6e4b22
Hans Roh <hansroh@gmail.com>
2019-05-12 23:30:05 +0000

    pypi

--- aquests/__init__.py
+++ aquests/__init__.py
@@ -1,6 +1,6 @@
 # 2016. 1. 10 by Hans Roh hansroh@gmail.com
 
-__version__ = "0.8.10.2"
+__version__ = "0.8.10.3"
 
 version_info = tuple (map (lambda x: not x.isdigit () and x or int (x),  __version__.split (".")))
 
