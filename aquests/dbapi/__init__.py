ca26e3daff505b6492ab6cca4ee9ad162c6d650a
Hans Roh <hansroh@gmail.com>
2019-08-01 21:35:39 +0900

    add syn mongo redis

--- aquests/dbapi/__init__.py
+++ aquests/dbapi/__init__.py
@@ -6,6 +6,8 @@ DB_POSTGRESQL = "*postgresql"
 DB_SQLITE3 = "*sqlite3"
 DB_REDIS = "*redis"
 DB_MONGODB = "*mongodb"
+DB_SYN_REDIS = "*redis_syn"
+DB_SYN_MONGODB = "*mongodb_syn"
 
 def set_timeout (timeout):
 	for each in (asynmongo.AsynConnect, asynpsycopg2.AsynConnect, asynredis.AsynConnect):
--- aquests/dbapi/syndbi.py
+++ aquests/dbapi/syndbi.py
@@ -1,40 +1,94 @@
 from .synsqlite3 import SynConnect
 from .dbconnect import DBConnect
+import psycopg2
+import redis
 
 class Postgres (SynConnect):
-	def connect (self):
-		try:
-			host, port = self.address		
-			self.conn = psycopg2.connect (
-				dbname = self.dbname,
-				user = self.user,
-				password = self.password,
-				host = host,
-				port = port
-			)
-		except:
-			self.handle_error ()
-		else:	
-			self.connected = True
+    def connect (self):
+        try:
+            host, port = self.address        
+            self.conn = psycopg2.connect (
+                dbname = self.dbname,
+                user = self.user,
+                password = self.password,
+                host = host,
+                port = port
+            )
+        except:
+            self.handle_error ()
+        else:    
+            self.connected = True
 
     def close_if_over_keep_live (self):
-		DBConnect.close_if_over_keep_live (self)	
+        DBConnect.close_if_over_keep_live (self)
 
     def execute (self, request):
-		dbconnect.DBConnect.begin_tran (self, request)			
-		sql = self._compile (request)
-		
-		if not self.connected:
-			self.connect ()
-			self.conn.isolation_level = None
-				
-		try:
-			if self.cur is None:
-				self.cur = self.conn.cursor ()
-				self.cur.execute (sql, *request.params [1:])
-				self.has_result = True
-		except:
-			self.handle_error ()
-		else:			
-			self.close_case ()
+        DBConnect.begin_tran (self, request)            
+        sql = self._compile (request)
+        
+        if not self.connected:
+            self.connect ()
+            self.conn.isolation_level = None
+                
+        try:
+            if self.cur is None:
+                self.cur = self.conn.cursor ()
+                self.cur.execute (sql, *request.params [1:])
+                self.has_result = True
+        except:
+            self.handle_error ()
+        else:            
+            self.close_case ()
 
+
+class Redis (Postgres):
+    def connect (self):
+        host, port = self.address
+        self.conn = redis.Redis (host = host, port = port, db = self.dbname)
+
+    def close (self, deactive = 1):    
+        if self.conn:    
+            self.conn.close ()            
+            self.conn = None    
+        self.connected = False    
+        DBConnect.close (self, deactive)
+
+    def _fetchall (self, command):
+        try:
+            resp = getattr (self.conn, command) (*self.request.params)
+        except:
+            self.handle_error ()
+        else:            
+            self.close_case ()
+        self.has_result = False    
+        return resp
+
+    def fetchall (self):
+        return self._fetchall (self.request.method)
+
+    def execute (self, request):
+        DBConnect.begin_tran (self, request)            
+        if not self.connected:
+            self.has_result = True
+            self.connect ()
+
+
+class MongoDB (Redis):
+    def fetchall (self):
+        return self._fetchall (self.request.method.lower ())        
+
+    def connect (self):
+        user, password = "", ""
+        auth = self.request.auth
+        if auth:
+            if len (auth) == 2:
+                user, password = auth
+            else:
+                user = auth [0]    
+        host, port = self.address
+
+        kargs = {}
+        if user: kargs ["username"] = user
+        if password: kargs ["password"] = password        
+        if port: kargs ["port"] = port        
+        self.conn = pymongo.MongoClient (host = host, **kargs)
