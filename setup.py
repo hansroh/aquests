0be3613a50798f24b2bf4288c57001218c2d5b06
Hans Roh <hansroh@gmail.com>
2019-08-12 20:18:00 +0900

    remove console script

--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ classifiers = [
 
 packages = [
 	'aquests',	
-	'aquests.bin',
 	'aquests.client',
 	'aquests.dbapi',	
 	'aquests.protocols',
@@ -66,7 +65,7 @@ packages = [
 	'aquests.protocols.smtp',
 	'aquests.protocols.grpc',
 	'aquests.protocols.proxy',
-	'aquests.athreads'		
+	'aquests.athreads'
 ]
 
 package_dir = {'aquests': 'aquests'}
