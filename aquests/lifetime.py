8c9b7aa735c1d4d03b52e8380594c6b8076ade05
Hans Roh <hansroh@gmail.com>
2019-11-03 09:29:47 +0000

    add http client toll

--- aquests/lifetime.py
+++ aquests/lifetime.py
@@ -8,15 +8,16 @@ import socket
 import time
 import types
 from .protocols import dns
+from collections import deque
 
 try:
 	from pympler import muppy, summary, tracker
 except ImportError:
-	pass	
+	pass
 
 if os.name == "nt":
 	from errno import WSAENOTSOCK
-	
+
 _shutdown_phase = 0
 _shutdown_timeout = 30 # seconds per phase
 _exit_code = 0
@@ -29,23 +30,38 @@ _polling = 0
 _logger = None
 
 EXHAUST_DNS = True
-	
+
 class Maintern:
 	def __init__ (self):
 		self.q = []
-		
+		self.onces = []
+
+	def call_at (self, at, func, args = ()):
+		# at is time.monotonic ()
+		self.onces.append ((at, func, args))
+		self.onces = sorted (self.onces, key = lambda x: x [0])
+
+	def call_onces (self):
+		now = time.monotonic ()
+		while self.onces:
+			at = self.onces [0][0]
+			if at > now:
+				break
+			at, func, args = self.onces.pop (0)
+			func (*args)
+
 	def sched (self, interval, func, args = None):
 		now = time.time ()
 		self.q.append ((now + interval, interval, func, args))
 		self.q.sort (key = lambda x: x [0])
 		#bisect.insort (self.q, (now + interval, interval, func, args))
-	
+
 	def __call__ (self, now):
 		excutes = 0
 		for exetime, interval, func, args in self.q:
 			if exetime > now: break
 			excutes += 1
-			if args: 
+			if args:
 				func (now, *args)
 			else:
 				func (now)
@@ -55,16 +71,16 @@ class Maintern:
 			#bisect.insort (self.q, (now + interval, interval, func, args))
 			self.q.append ((now + interval, interval, func, args))
 			self.q.sort (key = lambda x: x [0])
-		
+
 def maintern_gc (now):
 	gc.collect ()
-	
+
 def maintern_zombie_channel (now, map = None):
 	global _killed_zombies
-	
+
 	if map is None:
 		map = asyncore.socket_map
-			
+
 	for channel in list(map.values()):
 		if hasattr (channel, "handle_timeout"):
 			try:
@@ -72,7 +88,7 @@ def maintern_zombie_channel (now, map = None):
 				iszombie = (now - channel.event_time) > channel.zombie_timeout
 			except AttributeError:
 				continue
-			if iszombie:				
+			if iszombie:
 				_killed_zombies += 1
 				try:
 					channel.handle_timeout ()
@@ -82,7 +98,7 @@ def maintern_zombie_channel (now, map = None):
 maintern = None
 def init (kill_zombie_interval = 10.0, logger = None):
 	global maintern, _logger
-	
+
 	_logger = logger
 	maintern = Maintern ()
 	maintern.sched (kill_zombie_interval, maintern_zombie_channel)
@@ -91,35 +107,35 @@ def init (kill_zombie_interval = 10.0, logger = None):
 summary_tracker = None
 def summary_track (now):
 	global summary_tracker
-	
+
 	all_objects = muppy.get_objects ()
 	#all_objects = muppy.filter (all_objects, Type = dict)
 	#for each in all_objects:
 	#	print (each)
 	sum1 = summary.summarize (all_objects)
 	summary.print_ (sum1)
-	print ('-' * 79)	
+	print ('-' * 79)
 	if summary_tracker is None:
 		summary_tracker = tracker.SummaryTracker ()
-	summary_tracker.print_diff ()	
-	
+	summary_tracker.print_diff ()
+
 def enable_memory_track (interval = 10.0):
-	global maintern	
-	maintern.sched (interval, summary_track)	
+	global maintern
+	maintern.sched (interval, summary_track)
 
 def shutdown (exit_code, shutdown_timeout = 30.0):
 	global _shutdown_phase
 	global _shutdown_timeout
 	global _exit_code
-	
+
 	if _shutdown_phase:
 		# aready entered
 		return
-		
+
 	if _shutdown_phase == 0:
 		_exit_code = exit_code
 		_shutdown_phase = 1
-		
+
 	_shutdown_timeout = shutdown_timeout
 
 def loop (timeout = 30.0):
@@ -128,34 +144,34 @@ def loop (timeout = 30.0):
 	global _exit_code
 	global _polling
 	global maintern
-	
+
 	if maintern is None:
 		init ()
-		
+
 	_shutdown_phase = 0
 	_shutdown_timeout = 30
-	_exit_code = 0	
+	_exit_code = 0
 	_polling = 1
-	
-	try: 
+
+	try:
 		lifetime_loop(timeout)
 	except KeyboardInterrupt:
 		graceful_shutdown_loop()
 	else:
 		graceful_shutdown_loop()
-	
+
 	_polling = 0
-	
+
 poll_fun = asyncore.poll
 
 def remove_notsocks (map):
 	global _select_errors, _logger
-	
+
 	# on Windows we can get WSAENOTSOCK if the client
 	# rapidly connect and disconnects
 	killed = 0
-	for fd, obj in list(map.items()):	
-		r = []; w = []; e = []	
+	for fd, obj in list(map.items()):
+		r = []; w = []; e = []
 		is_r = obj.readable()
 		is_w = obj.writable()
 		if is_r:
@@ -165,49 +181,49 @@ def remove_notsocks (map):
 			w = [fd]
 		if is_r or is_w:
 			e = [fd]
-		
-		try:			
-			select.select (r, w, e, 0)			
-									
+
+		try:
+			select.select (r, w, e, 0)
+
 		except:
-			#_logger and _logger.trace ()			
+			#_logger and _logger.trace ()
 			killed += 1
 			_select_errors += 1
-						
+
 			try:
 				try: obj.handle_expt ()
 				except: obj.handle_error ()
 			except:
 				_logger and _logger.trace ()
-				
+
 			try: del map [fd]
 			except KeyError: pass
-					
+
 	return killed
 
 def poll_fun_wrap (timeout, map = None):
 	global _logger
 
 	if map is None:
-		map = asyncore.socket_map	
-	
+		map = asyncore.socket_map
+
 	if EXHAUST_DNS:
-		dns.pop_all ()	
-	
-	try:		
+		dns.pop_all ()
+
+	try:
 		poll_fun (timeout, map)
 	except (TypeError, OSError) as why:
 		# WSAENOTSOCK
 		remove_notsocks (map)
-	
+
 	except ValueError:
 		# negative file descriptor, testing all sockets
 		killed = remove_notsocks (map)
 		# or too many file descriptors in select(), divide and conquer
-		if not killed:			
+		if not killed:
 			half = int (len (map) / 2)
 			tmap = {}
-			cc = 0			
+			cc = 0
 			for k, v in list(map.items ()):
 				tmap [k] = v
 				cc += 1
@@ -215,33 +231,34 @@ def poll_fun_wrap (timeout, map = None):
 					poll_fun_wrap (timeout, tmap)
 					tmap = {}
 			poll_fun_wrap (timeout, tmap)
-	
+
 	except:
 		_logger and _logger.trace ()
 		raise
-		
+
 def lifetime_loop (timeout = 30.0, count = 0):
 	global _last_maintern
 	global _maintern_interval
 
 	loop = 0
 	map = asyncore.socket_map
-	
-	while map and _shutdown_phase == 0:		
+
+	while map and _shutdown_phase == 0:
 		poll_fun_wrap (timeout, map)
+		maintern.onces and maintern.call_onces ()
 		now = time.time()
 		if (now - _last_maintern) > _maintern_interval:
 			maintern (now)
 			_last_maintern = time.time ()
 		loop += 1
 		if count and loop > count:
-			break	
-		
+			break
+
 def graceful_shutdown_loop ():
 	global _shutdown_phase
 	timestamp = time.time()
 	timeout = 1.0
-	map = asyncore.socket_map	
+	map = asyncore.socket_map
 	while map and _shutdown_phase < 4:
 		time_in_this_phase = time.time() - timestamp
 		veto = 0
@@ -253,11 +270,11 @@ def graceful_shutdown_loop ():
 			else:
 				try:
 					veto = veto or fn (_shutdown_phase, time_in_this_phase)
-				except:					
+				except:
 					obj.handle_error()
-					
+
 		if veto and time_in_this_phase < _shutdown_timeout:
-			poll_fun_wrap (timeout, map)					
+			poll_fun_wrap (timeout, map)
 		else:
 			_shutdown_phase += 1
 			timestamp = time.time()
---
+++ aquests/protocols/http3/requests.py
@@ -0,0 +1,336 @@
+import argparse
+import asyncio
+import json
+import logging
+import pickle
+import ssl
+import sys
+import time
+from collections import deque
+from typing import Callable, Deque, Dict, List, Optional, Union, cast
+from urllib.parse import urlparse
+import os
+import wsproto
+import wsproto.events
+
+from aioquic.asyncio.client import connect
+from aioquic.asyncio.protocol import QuicConnectionProtocol
+from aioquic.h0.connection import H0_ALPN, H0Connection
+from aioquic.h3.connection import H3_ALPN, H3Connection
+from aioquic.h3.events import DataReceived, H3Event, HeadersReceived
+from aioquic.quic.configuration import QuicConfiguration
+from aioquic.quic.events import QuicEvent
+from aioquic.quic.logger import QuicLogger
+
+try:
+    import uvloop
+except ImportError:
+    uvloop = None
+
+logger = logging.getLogger("client")
+
+HttpConnection = Union[H0Connection, H3Connection]
+
+
+class URL:
+    def __init__(self, url: str):
+        parsed = urlparse(url)
+
+        self.authority = parsed.netloc
+        self.full_path = parsed.path
+        if parsed.query:
+            self.full_path += "?" + parsed.query
+        self.scheme = parsed.scheme
+
+
+class HttpRequest:
+    def __init__(
+        self, method: str, url: URL, content: bytes = b"", headers: Dict = {}
+    ) -> None:
+        self.content = content
+        self.headers = headers
+        self.method = method
+        self.url = url
+
+
+class WebSocket:
+    def __init__(
+        self, http: HttpConnection, stream_id: int, transmit: Callable[[], None]
+    ) -> None:
+        self.http = http
+        self.queue: asyncio.Queue[str] = asyncio.Queue()
+        self.stream_id = stream_id
+        self.subprotocol: Optional[str] = None
+        self.transmit = transmit
+        self.websocket = wsproto.Connection(wsproto.ConnectionType.CLIENT)
+
+    async def close(self, code=1000, reason="") -> None:
+        """
+        Perform the closing handshake.
+        """
+        data = self.websocket.send(
+            wsproto.events.CloseConnection(code=code, reason=reason)
+        )
+        self.http.send_data(stream_id=self.stream_id, data=data, end_stream=True)
+        self.transmit()
+
+    async def recv(self) -> str:
+        """
+        Receive the next message.
+        """
+        return await self.queue.get()
+
+    async def send(self, message: str):
+        """
+        Send a message.
+        """
+        assert isinstance(message, str)
+
+        data = self.websocket.send(wsproto.events.TextMessage(data=message))
+        self.http.send_data(stream_id=self.stream_id, data=data, end_stream=False)
+        self.transmit()
+
+    def http_event_received(self, event: H3Event):
+        if isinstance(event, HeadersReceived):
+            for header, value in event.headers:
+                if header == b"sec-websocket-protocol":
+                    self.subprotocol = value.decode("utf8")
+        elif isinstance(event, DataReceived):
+            self.websocket.receive_data(event.data)
+
+        for ws_event in self.websocket.events():
+            self.websocket_event_received(ws_event)
+
+    def websocket_event_received(self, event: wsproto.events.Event) -> None:
+        if isinstance(event, wsproto.events.TextMessage):
+            self.queue.put_nowait(event.data)
+
+
+class HttpClient(QuicConnectionProtocol):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._http: Optional[HttpConnection] = None
+        self._request_events: Dict[int, Deque[H3Event]] = {}
+        self._request_waiter: Dict[int, asyncio.Future[Deque[H3Event]]] = {}
+        self._websockets: Dict[int, WebSocket] = {}
+
+        if self._quic.configuration.alpn_protocols[0].startswith("hq-"):
+            self._http = H0Connection(self._quic)
+        else:
+            self._http = H3Connection(self._quic)
+
+    async def get(self, url: str, headers: Dict = {}) -> Deque[H3Event]:
+        """
+        Perform a GET request.
+        """
+        print ('------- url', url)
+        return await self._request(
+            HttpRequest(method="GET", url=URL(url), headers=headers)
+        )
+
+    async def post(self, url: str, data: bytes, headers: Dict = {}) -> Deque[H3Event]:
+        """
+        Perform a POST request.
+        """
+        return await self._request(
+            HttpRequest(method="POST", url=URL(url), content=data, headers=headers)
+        )
+
+    async def websocket(self, url: str, subprotocols: List[str] = []) -> WebSocket:
+        """
+        Open a WebSocket.
+        """
+        request = HttpRequest(method="CONNECT", url=URL(url))
+        stream_id = self._quic.get_next_available_stream_id()
+        websocket = WebSocket(
+            http=self._http, stream_id=stream_id, transmit=self.transmit
+        )
+
+        self._websockets[stream_id] = websocket
+
+        headers = [
+            (b":method", b"CONNECT"),
+            (b":scheme", b"https"),
+            (b":authority", request.url.authority.encode("utf8")),
+            (b":path", request.url.full_path.encode("utf8")),
+            (b":protocol", b"websocket"),
+            (b"user-agent", b"aioquic"),
+            (b"sec-websocket-version", b"13"),
+        ]
+        if subprotocols:
+            headers.append(
+                (b"sec-websocket-protocol", ", ".join(subprotocols).encode("utf8"))
+            )
+        self._http.send_headers(stream_id=stream_id, headers=headers)
+
+        self.transmit()
+
+        return websocket
+
+    def http_event_received(self, event: H3Event):
+        if isinstance(event, (HeadersReceived, DataReceived)):
+            stream_id = event.stream_id
+            if stream_id in self._request_events:
+                # http
+                self._request_events[event.stream_id].append(event)
+                if event.stream_ended:
+                    request_waiter = self._request_waiter.pop(stream_id)
+                    request_waiter.set_result(self._request_events.pop(stream_id))
+
+            elif stream_id in self._websockets:
+                # websocket
+                websocket = self._websockets[stream_id]
+                websocket.http_event_received(event)
+
+            else:
+                # push
+                print(event)
+
+    def quic_event_received(self, event: QuicEvent):
+        #  pass event to the HTTP layer
+        if self._http is not None:
+            for http_event in self._http.handle_event(event):
+                self.http_event_received(http_event)
+
+    async def _request(self, request: HttpRequest):
+        stream_id = self._quic.get_next_available_stream_id()
+        self._http.send_headers(
+            stream_id=stream_id,
+            headers=[
+                (b":method", request.method.encode("utf8")),
+                (b":scheme", request.url.scheme.encode("utf8")),
+                (b":authority", request.url.authority.encode("utf8")),
+                (b":path", request.url.full_path.encode("utf8")),
+                (b"user-agent", b"aioquic"),
+            ]
+            + [
+                (k.encode("utf8"), v.encode("utf8"))
+                for (k, v) in request.headers.items()
+            ],
+        )
+        self._http.send_data(stream_id=stream_id, data=request.content, end_stream=True)
+
+        waiter = self._loop.create_future()
+        self._request_events[stream_id] = deque()
+        self._request_waiter[stream_id] = waiter
+        self.transmit()
+
+        return await asyncio.shield(waiter)
+
+
+def save_session_ticket(ticket):
+    """
+    Callback which is invoked by the TLS engine when a new session ticket
+    is received.
+    """
+    logger.info("New session ticket received")
+    if args.session_ticket:
+        with open(args.session_ticket, "wb") as fp:
+            pickle.dump(ticket, fp)
+
+
+class Response:
+    headers = b''
+    data = b''
+
+async def run(configuration: QuicConfiguration, url: str, data: str, response: Response) -> None:
+    # parse URL
+    parsed = urlparse(url)
+    assert parsed.scheme in (
+        "https",
+        "wss",
+    ), "Only https:// or wss:// URLs are supported."
+    if ":" in parsed.netloc:
+        host, port_str = parsed.netloc.split(":")
+        port = int(port_str)
+    else:
+        host = parsed.netloc
+        port = 443
+
+    async with connect(
+        host,
+        port,
+        configuration=configuration,
+        create_protocol=HttpClient,
+        session_ticket_handler=save_session_ticket,
+    ) as client:
+        client = cast(HttpClient, client)
+
+        if parsed.scheme == "wss":
+            ws = await client.websocket(url, subprotocols=["chat", "superchat"])
+
+            # send some messages and receive reply
+            for i in range(2):
+                message = "Hello {}, WebSocket!".format(i)
+                print("> " + message)
+                await ws.send(message)
+
+                message = await ws.recv()
+                print("< " + message)
+
+            await ws.close()
+        else:
+            # perform request
+            start = time.time()
+            if data is not None:
+                http_events = await client.post(
+                    url,
+                    data=data.encode("utf8"),
+                    headers={"content-type": "application/x-www-form-urlencoded"},
+                )
+            else:
+                http_events = await client.get(url)
+            elapsed = time.time() - start
+
+            # print speed
+            octets = 0
+            for http_event in http_events:
+                if isinstance(http_event, DataReceived):
+                    octets += len(http_event.data)
+            logger.info(
+                "Received %d bytes in %.1f s (%.3f Mbps)"
+                % (octets, elapsed, octets * 8 / elapsed / 1000000)
+            )
+
+            # print response
+            for http_event in http_events:
+                if isinstance(http_event, HeadersReceived):
+                    headers = {}
+                    for k, v in http_event.headers:
+                        headers [k.decode ()] = v.decode ()
+                    response.headers = headers
+                elif isinstance(http_event, DataReceived):
+                    response.data = http_event.data
+
+
+def _request (url, data = None):
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s %(name)s %(message)s",
+        level=logging.INFO,
+    )
+    # prepare configuration
+    configuration = QuicConfiguration(
+        is_client=True, alpn_protocols=H3_ALPN
+    )
+    configuration.load_verify_locations(os.path.join (os.path.dirname (__file__), 'pycacert.pem'))
+    configuration.verify_mode = ssl.CERT_NONE
+    loop = asyncio.get_event_loop()
+    response = Response ()
+    loop.run_until_complete(
+        run(configuration=configuration, url=url, data=data, response=response)
+    )
+    return response
+
+def get (url):
+    return _request (url)
+
+def post (url, data):
+    return _request (url, data)
+
+
+if __name__ == "__main__":
+    r = get ('https://localhost:4433/')
+    print (r.headers)
+    print (r.data)
+
